9f6bcab5d15b4a5bfaa9730ea38d841bd5ab4130
dhilshana <dhldhilshana@gmail.com>
2025-08-06 21:37:39 +0530

    setup color scheme for dark and light modes

---
+++ lib/utils/theme.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColors{
+
+  static const primaryColorLight = Color(0xFF2196F3);
+  static const primaryColorDark = Color(0xFF2196F3);
+  static const secondaryColorLight = Color(0xFF9CA3AF);
+  static const secondaryColorDark = Color(0xFFD1D5DB);
+
+  static const lightBackground = Color(0xFFFFFFFF);
+  static const darkBackground = Color(0xFF111827);
+  static const lightSurfaceColor = Color(0xFFF8F9FA);
+  static const darkSurfaceColor = Color(0xFF1F2937);
+
+  static const lightTextColor = Color(0xFF000000);
+  static const darkTextColor = Color(0xFFFFFFFF);
+
+  // Success Green
+  static const successGreenLight = Color(0xFF049668);
+  static const successGreenDark = Color(0xFF4ADE80);
+  static const successGreenBgLight = Color(0xFFE9F5E8);
+  static const successGreenBgDark = Color(0xFF052E16);
+
+  // Warning Yellow
+  static const warningYellowLight = Color(0xFFF69C3C);
+  static const warningYellowDark = Color(0xFFFDE047);
+  static const warningYellowBgLight = Color(0xFFFFFAEA);
+  static const warningYellowBgDark = Color(0xFF3F3F06);
+
+  static const primaryBlueBgLight = Color(0xFFE3F3FC);
+  static const primaryBlueBgDark = Color(0xFF1E3A8A);
+
+  // Violet
+  static const violetLight = Color(0xFFAF66EE);
+  static const violetDark = Color(0xFFA78BFA);
+
+  // red
+  static const redLight = Color(0xFFE14646);
+
+}
