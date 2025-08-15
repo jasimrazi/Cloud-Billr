792cca456df9d593fb9d6cd5879b042f8b49c67c
jasimrazi <jasimrazi17@gmail.com>
2025-08-15 21:48:26 +0530

    test: second commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 //Entry point
+//new entry point
 void main() {
   runApp(MultiProvider(
     providers: providers,
