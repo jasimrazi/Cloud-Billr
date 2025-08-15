d1c36c3b5b90de2b58990a7c63b21bb331e6dd84
jasimrazi <jasimrazi17@gmail.com>
2025-08-15 21:48:26 +0530

    test: second commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:cloud_billr/controllers/providers.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+//Entry point
+//new entry point
 void main() {
   runApp(MultiProvider(
     providers: providers,
