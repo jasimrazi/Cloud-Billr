3faa57fa500a6996764dee48f850055efd600758
jasimrazi <jasimrazi17@gmail.com>
2026-07-02 22:15:24 +0530

    feat: implement ThemeProvider and integrate theme management into the app

---
+++ lib/controllers/theme_provider.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  ThemeMode _themeMode = ThemeMode.system;
+
+  ThemeMode get themeMode => _themeMode;
+
+  bool get isDarkMode => _themeMode == ThemeMode.dark;
+
+  void setThemeMode(ThemeMode mode) {
+    if (_themeMode != mode) {
+      _themeMode = mode;
+      notifyListeners();
+    }
+  }
+
+  void toggleTheme(bool isDark) {
+    setThemeMode(isDark ? ThemeMode.dark : ThemeMode.light);
+  }
+}
